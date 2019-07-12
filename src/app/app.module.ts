1a6dff3c6254ddc958c5db7f39dc5e870277e0f9
lindthon <lindthon4@hotmail.com>
2019-07-12 07:04:49 -0500

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
+import { BuscarObjetoPerdidoComponent } from './buscar-objeto-perdido/buscar-objeto-perdido.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { DetallesComponent } from './detalles/detalles.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { InformacionDetalladaComponent } from './informacion-detallada/informacion-detallada.component';
+import { PerdidosComponent } from './perdidos/perdidos.component';
+import { RegistrarObjetoPerdidoComponent } from './registrar-objeto-perdido/registrar-objeto-perdido.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, IndexComponent, BuscarObjetoPerdidoComponent, ContactoComponent, DetallesComponent, EquipoComponent, InformacionDetalladaComponent, PerdidosComponent, RegistrarObjetoPerdidoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
